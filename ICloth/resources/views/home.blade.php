3247bfaf829112e9686a26e11e66c76764e0cdff
jonathas-sousa <jonathas@scadiagro.com.br>
2022-04-17 20:59:47 -0300

    Tela Inicial
    Close #19

--- ICloth/resources/views/home.blade.php
+++ ICloth/resources/views/home.blade.php
@@ -1,3 +1,8 @@
 <x-app>
-
+    <div class="btn-link">
+        <a href="{{route('venda.index')}}" class="btn col-1"><img src="https://s3-alpha-sig.figma.com/img/1e08/fd80/fbbb29b32396a8e2a796c4c9674fff37?Expires=1650844800&Signature=BpBJQT4t2BM0oQzYDnfDh1Xae0-3Z1QMJqVdC4wXFVSi5Z8b7HJwPtrRekniDCZfYJtwc7n6vcDk8t5bwEvXZSM6f~ToOrRLxJgUmVA6b-yQTf4piXO0vJXyjpLq5UARqP26lBRVI1-nI0tFVZrkpiQKHOL1YKsKQgWWoF5XtpDrybJ50R2iZ99zRpCC2fkR7-hNAy4ai~z4ZjSO5eU9xC2KDTISxY0J5VZH1WaS8jvAOQajyGw8LKWOGVYerf6sZXTwVj4qcJ1KqwHrS4CAooyyAFVlRUUPK5fUPI6SvONK~zlErY4ZEcbec8rOKP7pq1RkDpohVZZUVz2hgZ-WMA__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA" alt=""><br><p>Vendas</p></a>
+        <a href="{{route('cliente.index')}}" class="btn col-1"><img src="https://s3-alpha-sig.figma.com/img/771b/d849/4f79a4a2628b2a82c008b0b223481bf5?Expires=1650844800&Signature=H2KfVfCe8mIb7qSGcvoNyPBwYYPqJxTn5w~9x4t49HFaEuddRJj51jqUCUas4GS0Q5OQHiYPrycikk0XCaj6yOMXihFZ6-~pxxq4fvRxWpY~SrsTTQ-9thXbMB19tNGgn0ZUB-1nBcBzoMhDR~vvvWoMbL8b0E8DhujDKpRVqk-~tpHFDGRfv3YhxRyIl6hKKTi59ZF3UGazNYX2Q2GxMh7iTEP3C3vdxkVOeKU83F~TB7uN1KdtwHV0JEfJ5-yoGl2EVV5wqF8wj5ZbuWKE83Sjucj0uNb~mrhiFbhLp5HUsuTbYrGdMUNIpxHnnM9BOQCXtOq~-YjJ2~i~T4KREw__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA" alt=""><br><p>Clientes</p></a>
+        <a href="{{route('produto.index')}}" class="btn col-1"><img src="https://s3-alpha-sig.figma.com/img/aa7d/e189/2db150860e772aaf3108c7eff9bcb915?Expires=1650844800&Signature=VQSYUHthyByz9~UiYysxNxjQMBQYRANQZudnHDooAD2jUaPT890cAqwbzClm-wrpkH0G9ZLqneDUOsxyvm~ogx6zKtxj89kbI9wXl6cF0VXwurW-JC-kYxl4LrYx-goYJd38oWWH1YnMQRTgBzQ68tDGUeM4ybwUR5yticXxT-g-Vh6Ivj2i6al5TpCrjASGznzSOKiGpSTWPIvzPwL56D7dkcpqy4yk3nSB9rJSa8uL6ZUXnt3KQj6fxTTdixUzhgC0BZ1fFTvC3pizzWMPg8AYYozT2Z2BKdp2qznzM-0VSwUYLAVhx~8IoTJIoIKYCxgEuKXqvGikfXI-jouFaQ__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA" alt=""><br><p>Produtos</p></a>
+        
+    </div>
 </x-app>
